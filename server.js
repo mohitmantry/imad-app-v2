90101353e8b645d8e8b90fc305058c39d276bcdb
mohitmantry <mohitman2222@gmail.com>
2017-02-15 11:15:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article1', function (req, res) {
-  res.send('ARTICLE 1 REQUESTED AND WILL BE SERVED HERE!!!');
+  res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
 });
 app.get('/article2', function (req, res) {
   res.send('ARTICLE 2 REQUESTED AND WILL BE SERVED HERE!!!');
